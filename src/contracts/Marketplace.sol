9d29e216a1150b3a739a92391c4d81a9cb3896e1
piyushgaikwad <55937806+piyushgaikwad@users.noreply.github.com>
2021-05-10 14:28:14 -0700

    Update Marketplace.sol

--- src/contracts/Marketplace.sol
+++ src/contracts/Marketplace.sol
@@ -30,7 +30,7 @@ contract Marketplace {
     );
 
     constructor() public {
-        name = "Dapp University Marketplace";
+        name = "Piyush gaikwad";
     }
 
     function createProduct(string memory _name, uint _price) public {
